2f870f0bd627bf7e081e61b59d0d11b0aa12e162
Blair Bristow <blairhbristow@gmail.com>
2022-03-23 16:52:54 -0400

    Edit solution 12

---
+++ exercises/12/index.js
@@ -0,0 +1,7 @@
+const div = document.querySelector("div.queens")
+const ul = document.createElement("ul")
+const li = document.createElement("li")
+li.innerHTML = "Kiki"
+
+ul.appendChild(li)
+div.appendChild(ul)
